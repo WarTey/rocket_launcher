2287157d3bbee22aee1bc6888ad7de529a1c454d
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-12-27 22:31:34 +0100

    Level design

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -24,6 +24,20 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
